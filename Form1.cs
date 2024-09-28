a97de1afc7fcfedbdba399d7c580e19e49064e1b
VonguyenMinhNhat <160804783+VonguyenMinhNhat@users.noreply.github.com>
2024-09-28 18:00:50 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitaptuan5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnADD_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView1.Rows.Add(frm.MaNV, frm.TenNV, frm.LuongCB);
+            }
+        }
+
+        private void btnfix_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0) // Kiểm tra có mục nào được chọn không
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                // Cập nhật thông tin từ các TextBox
+                Form2 frm = new Form2();
+                if (frm.ShowDialog() == DialogResult.OK)
+                {
+                    selectedRow.Cells["col_MaNV"].Value = frm.MaNV;
+                    selectedRow.Cells["col_TenNV"].Value = frm.TenNV;
+                    selectedRow.Cells["col_Luong"].Value = frm.LuongCB;
+
+                    frm.ClearFields();
+                }
+            }
+            else { MessageBox.Show("Vui lòng chọn một nhân viên để sửa."); }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn mục để xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitaptuan5
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public string MaNV { get; set; }
+        public string TenNV { get; set; }
+        public int LuongCB { get; set; }
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Yes_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox2.Text) || string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox3.Text))
+            {
+                MessageBox.Show("Vui Lòng Nhập Đủ Thông Tin!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                MaNV = textBox2.Text;
+                TenNV = textBox1.Text;
+                LuongCB = int.Parse(textBox3.Text);
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+        public void ClearFields()
+        {
+            textBox2.Clear();
+            textBox1.Clear();
+            textBox3.Clear();
+        }
+
+        private void btn_Skip_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
